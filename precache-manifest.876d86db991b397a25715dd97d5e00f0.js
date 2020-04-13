5b70ddfdf9261b2bd1839306fa37e938830d3056
hxy <491746926@qq.com>
2020-04-13 22:22:37 +0800

    first

---
+++ precache-manifest.876d86db991b397a25715dd97d5e00f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdf8977c667ba40746477f878efb8e3",
+    "url": "/editor/index.html"
+  },
+  {
+    "revision": "1787f1b61cd50ac78771",
+    "url": "/editor/static/css/main.388e1a0c.chunk.css"
+  },
+  {
+    "revision": "783f7fd19b7f541fcde8",
+    "url": "/editor/static/js/2.8e8e27bb.chunk.js"
+  },
+  {
+    "revision": "1787f1b61cd50ac78771",
+    "url": "/editor/static/js/main.a442d6e6.chunk.js"
+  },
+  {
+    "revision": "7f83cd211924f8f90037",
+    "url": "/editor/static/js/runtime~main.1594e5d4.js"
+  }
+]);
